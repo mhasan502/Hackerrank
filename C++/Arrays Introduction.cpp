d5b0a658fa9bae165c832d59ba5aedf67b87ec6c
Mehedi Hasan <mhasan502@gmail.com>
2019-10-14 13:04:59 +0600

    Create Arrays Introduction.cpp

---
+++ C++/Arrays Introduction.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int size;
+    cin >> size;
+    int array[size];
+    for(int i=0;i<size;i++){
+        cin >> array[i]; 
+    }
+    for(int i=size-1;i>=0;i--){
+        cout << array[i] << " ";
+    }
+    return 0;
+}
