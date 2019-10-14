02eeb467f753c6ab2ee726e0311fa55bb614a408
mhasan502 <mhasan502@gmail.com>
2019-10-14 16:01:34 +0600

    Update C++ Class Templates.cpp

--- C++/C++ Class Templates.cpp
+++ C++/C++ Class Templates.cpp
@@ -6,24 +6,29 @@
 #include <cassert>
 using namespace std;
 
-template <class T>
-    class AddElements {
-        public:
+template <class T> class AddElements {
+    public:
         T element;
-        AddElements (T arg) {element=arg;}
-        T add(T el2) {
-            return element + el2;
+        AddElements(T i) {
+	element = i;
         }
-    };
-    template <>
-    class AddElements <string> {
-        public:
+        T add(T i) {
+	return element+i;
+        }
+        
+};
+template <> class AddElements <string> {
+    public:
         string element;
-        AddElements (string arg) {element=arg;}
-        string concatenate (string el2) {
-            return element + el2;
+        AddElements(string i) {
+	element = i;
         }
-    };
+        string concatenate(string element2) {
+	return element+element2;
+        }
+        
+};
+  
 
 int main () {
   int n,i;
