5cb92930e2272e3330f514887b36222a10e2b2ed
Mehedi Hasan <mhasan502@gmail.com>
2019-10-22 21:32:46 +0600

    Create Java Map.java

---
+++ Java/Java Map.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+   public static void main(String []argh) {
+      Scanner in = new Scanner(System.in);
+      int n=in.nextInt();
+      in.nextLine();
+      HashMap<String, Integer> h = new HashMap<String, Integer>();
+      
+      for(int i=0;i<n;i++)  {
+         String name=in.nextLine();
+         int phone=in.nextInt();
+         h.put(name, phone);         
+         in.nextLine();
+      }
+      while(in.hasNext()) {
+         String s=in.nextLine();
+         try{
+            int temp = h.get(s);
+            System.out.println(s+"="+temp);
+         }catch(NullPointerException e){
+            System.out.println("Not found");
+         }    
+      }
+   }
+}
