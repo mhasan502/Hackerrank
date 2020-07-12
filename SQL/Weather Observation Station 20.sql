312f81f24e374ab6ffbc3ab494b26e25b0c12857
Mehedi Hasan <mhasan502@gmail.com>
2020-07-12 22:42:06 +0600

    Create Weather Observation Station 20.sql

---
+++ SQL/Weather Observation Station 20.sql
@@ -0,0 +1,6 @@
+SELECT ROUND(S1.LAT_N,4)
+FROM STATION AS S1
+WHERE (SELECT ROUND(COUNT(S1.ID)/2)-1 
+       FROM STATION) = (SELECT COUNT(S2.ID)
+                        FROM STATION S2
+                        WHERE S2.LAT_N > S1.LAT_N)
